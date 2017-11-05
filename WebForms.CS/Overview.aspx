2e31c8678041a092715915d70b58969615c7ff10
huangjy <huangjianyong@yikangtech.com>
2017-11-05 10:14:23 +0800

    添加项目文件。

---
+++ WebForms.CS/Overview.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Overview.aspx.cs" Inherits="GleamTech.ImageUltimateExamples.WebForms.CS.OverviewPage" %>
+<%@ Register TagPrefix="GleamTech" Namespace="GleamTech.ExamplesCore" Assembly="GleamTech.ExamplesCore" %>
+<%@ Import Namespace="GleamTech.ImageUltimate.Web" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>Overview</title>
+    <link href="<%=ExamplesCoreConfiguration.GetVersionedUrl("~/resources/table.css")%>" rel="stylesheet" />
+</head>
+<body style="margin: 20px;">
+
+    <GleamTech:ExampleFileSelector ID="exampleFileSelector" runat="server"
+        InitialFile="JPG Image.jpg" />
+    
+    <table class="info image">
+        <caption>Thumbnail</caption>
+        <tr>
+            <td><%=Page.ImageHtml(ImagePath, task => task.Thumbnail(160))%></td>
+        </tr>
+    </table>
+
+    <table class="info">
+        <caption>Image Info</caption>
+        <% foreach (var kvp in ImageData) {%>
+        <tr>
+            <th><%=kvp.Key%></th>
+            <td><%=kvp.Value%></td>
+        </tr>
+        <%}%>
+    </table>
+    
+    <table class="info">
+        <caption>Image EXIF Metadata</caption>
+        <% foreach (var kvp in ImageExifMetadata) {%>
+        <tr>
+            <th><%=kvp.Key%></th>
+            <td><%=kvp.Value.Item1%></td>
+            <td><%=kvp.Value.Item2%></td>
+        </tr>
+        <%}%>
+    </table>
+    
+    <table class="info">
+        <caption>Image IPTC Metadata</caption>
+        <% foreach (var kvp in ImageIptcMetadata) {%>
+        <tr>
+            <th><%=kvp.Key%></th>
+            <td><%=kvp.Value.Item1%></td>
+            <td><%=kvp.Value.Item2%></td>
+        </tr>
+        <%}%>
+    </table>
+
+</body>
+</html>
---
+++ WebForms.CS/Overview.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.UI;
+using GleamTech.ImageUltimate;
+
+namespace GleamTech.ImageUltimateExamples.WebForms.CS
+{
+    public partial class OverviewPage : Page
+    {
+        protected string ImagePath;
+        protected Dictionary<string, object> ImageData = new Dictionary<string, object>();
+        protected Dictionary<string, Tuple<string, string>> ImageExifMetadata = new Dictionary<string, Tuple<string, string>>();
+        protected Dictionary<string, Tuple<string, string>> ImageIptcMetadata = new Dictionary<string, Tuple<string, string>>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ImagePath = exampleFileSelector.SelectedFile;
+
+            using (var imageInfo = new ImageInfo(ImagePath))
+            {
+                ImageData.Add("Format", imageInfo.Format);
+                ImageData.Add("Width", imageInfo.Width);
+                ImageData.Add("Height", imageInfo.Height);
+                ImageData.Add("DpiX", imageInfo.DpiX);
+                ImageData.Add("DpiY", imageInfo.DpiY);
+                ImageData.Add("PixelFormat", imageInfo.PixelFormatInfo.Description);
+                ImageData.Add("⤷ ColorModel", imageInfo.PixelFormatInfo.ColorModel);
+                ImageData.Add("⤷ BitDepth", imageInfo.PixelFormatInfo.BitDepth);
+                ImageData.Add("⤷ HasAlpha", imageInfo.PixelFormatInfo.HasAlpha);
+                ImageData.Add("⤷ IsIndexed", imageInfo.PixelFormatInfo.IsIndexed);
+                ImageData.Add("⤷ IsRgb", imageInfo.PixelFormatInfo.IsRgb);
+                ImageData.Add("⤷ IsExtended", imageInfo.PixelFormatInfo.IsExtended);
+                ImageData.Add("⤷ ChannelCount", imageInfo.PixelFormatInfo.ChannelCount);
+                ImageData.Add("⤷ MaxChannelValue", imageInfo.PixelFormatInfo.MaxChannelValue);
+
+                foreach (var entry in imageInfo.ExifDictionary)
+                    ImageExifMetadata.Add(entry.Key.ToString(), Tuple.Create(GetStringValue(entry), entry.Description));
+
+                if (ImageExifMetadata.Count == 0)
+                    ImageExifMetadata.Add("", Tuple.Create("", ""));
+
+                foreach (var entry in imageInfo.IptcDictionary)
+                    ImageIptcMetadata.Add(entry.Key.ToString(), Tuple.Create(GetStringValue(entry), entry.Description));
+
+                if (ImageIptcMetadata.Count == 0)
+                    ImageIptcMetadata.Add("", Tuple.Create("", ""));
+            }
+        }
+
+        private static string GetStringValue<TKey>(MetadataEntry<TKey> entry)
+        {
+            if (entry.Value is string)
+                return entry.ValueString;
+
+            var valueType = entry.Value.GetType();
+            if (valueType.IsArray)
+                return string.Format("{0} array with length {1}", valueType.GetElementType(), ((ICollection)entry.Value).Count);
+
+            if (entry.Values.Length > 1)
+                return string.Format("{0} array with length {1}", entry.Values[0].GetType(), entry.Values.Length);
+
+            return entry.ValueString;
+        }
+    }
+}
